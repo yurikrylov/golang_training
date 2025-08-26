9e163201783d0cea06b4c2da10ea34c3b07c45e7
Yuri Krylov <yuri.krylov@gmail.com>
2025-08-26 06:13:03 +0200

    day 3

--- main.go
+++ main.go
@@ -1,10 +1,21 @@
 package main
 
-import "github.com/yurikrylov/golang_training/utils"
+import "fmt"
+
+// import "github.com/yurikrylov/golang_training/utils"
 
 func main() {
+
+	b := []byte{'G', 'o'}
+	s := string(b)
+	b[0] = 'g'
+	fmt.Println(s)
+
+}
+
+/*
 	var order = []string{"хлеб", "буженина", "сыр", "огурцы"}
 	utils.CalcOrder(order)
-	//utils.FizzBuzz()
-	//utils.Switch()
-}
+	utils.FizzBuzz()
+	utils.Switch()
+*/
